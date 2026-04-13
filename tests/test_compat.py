370cbd6dbd69ac737744b64f0f07e93918d1b495
pnuckowski <p.nuckowski@gmail.com>
2026-04-14 01:51:06 +0200

    add tests for normalize_url function and update README examples for pytest integration

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -4,7 +4,7 @@ from typing import Union
 import pytest
 from yarl import URL
 
-from aioresponses.compat import merge_params
+from aioresponses.compat import merge_params, normalize_url
 
 URL_WITH_PARAMS = 'http://example.com/api?foo=bar#fragment'
 URL_WITHOUT_PARAMS = 'http://example.com/api?#fragment'
@@ -36,3 +36,54 @@ def test_both_with_params_returns_corrected_url(as_str):
 def test_base_without_params_returns_corrected_url(as_str):
     url = get_url(URL_WITHOUT_PARAMS, as_str)
     assert merge_params(url, {'x': 42}) == URL('http://example.com/api?x=42#fragment')
+
+
+@pytest.mark.parametrize(
+    "original_url, expected_url, as_str",
+    [
+        (
+            "http://example.com/api?var2=baz&var1=foo",
+            "http://example.com/api?var1=foo&var2=baz",
+            as_str,
+        )
+        for as_str in [True, False]
+    ]
+    + [
+        (
+            "https://example.com/api?var3=gaz%3Bdar&var1=foo:bar&var1=bar/baz&var2=baz%26gaz",
+            "https://example.com/api?var1=bar%252Fbaz&var1=foo%253Abar&var2=baz%2526gaz&var3=gaz%253Bdar",
+            as_str,
+        )
+        for as_str in [True, False]
+    ]
+    + [
+        (
+            "https://example.com/api?var3=gaz%3Bdar&var1=foo%3Abar&var1=bar%2Fbaz&var2=baz%26gaz",
+            "https://example.com/api?var1=bar%252Fbaz&var1=foo%253Abar&var2=baz%2526gaz&var3=gaz%253Bdar",
+            as_str,
+        )
+        for as_str in [True, False]
+    ]
+    + [
+        (
+            "https://example.com/api?var=\u043f\u0443\u0442\u044c",
+            "https://example.com/api?var=%25D0%25BF%25D1%2583%25D1%2582%25D1%258C",
+            as_str,
+        )
+        for as_str in [True, False]
+    ]
+    + [
+        (
+            "https://example.com/api?var=%D0%BF%D1%83%D1%82%D1%8C",
+            "https://example.com/api?var=%25D0%25BF%25D1%2583%25D1%2582%25D1%258C",
+            as_str,
+        )
+        for as_str in [True, False]
+    ],
+)
+def test_normalize_url(original_url, expected_url, as_str):
+    original_url = get_url(original_url, as_str)
+    received_url = normalize_url(original_url)
+    assert isinstance(received_url, URL)
+    assert expected_url == str(received_url)
+
